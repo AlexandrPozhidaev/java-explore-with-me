2d439f7eea5daed69dbbe9f7cf6204eb8c52b545
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-07-15 01:39:30 +0300

    fix репозиторий - синтаксис

--- stat_srvc/src/main/java/ru/practicum/statsrvc/stat/StatRepository.java
+++ stat_srvc/src/main/java/ru/practicum/statsrvc/stat/StatRepository.java
@@ -12,24 +12,28 @@ import java.util.List;
 @Repository
 public interface StatRepository extends JpaRepository<Stat, Long> {
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(h)) " +
-            "FROM Stat h " +
-            "WHERE (:uris IS NULL OR h.uri IN :uris) " +
-            "AND h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri" +
-            "ORDER BY COUNT(h) DESC")
+    @Query("""
+        SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(h))
+        FROM Stat h
+        WHERE (:uris IS NULL OR h.uri IN :uris)
+          AND h.timestamp BETWEEN :start AND :end
+        GROUP BY h.app, h.uri
+        ORDER BY COUNT(h) DESC
+        """)
     List<ViewStatsDto> findAllStats(
             @Param("uris") List<String> uris,
             @Param("start") LocalDateTime start,
             @Param("end") LocalDateTime end
     );
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
-            "FROM Stat h " +
-            "WHERE (:uris IS NULL OR h.uri IN :uris) " +
-            "AND h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri" +
-            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+    @Query("""
+        SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip))
+        FROM Stat h
+        WHERE (:uris IS NULL OR h.uri IN :uris)
+          AND h.timestamp BETWEEN :start AND :end
+        GROUP BY h.app, h.uri
+        ORDER BY COUNT(DISTINCT h.ip) DESC
+        """)
     List<ViewStatsDto> findUniqueStats(
             @Param("uris") List<String> uris,
             @Param("start") LocalDateTime start,
