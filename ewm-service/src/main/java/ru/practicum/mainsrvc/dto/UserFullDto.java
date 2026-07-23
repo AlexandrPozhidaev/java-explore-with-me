6d3baaadc84dae702297f046bc72da45a2c3ec43
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-07-24 00:36:59 +0300

    fix

--- ewm-service/src/main/java/ru/practicum/mainsrvc/dto/UserFullDto.java
+++ ewm-service/src/main/java/ru/practicum/mainsrvc/dto/UserFullDto.java
@@ -23,15 +23,35 @@ public class UserFullDto {
         this.active = active;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
 
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
