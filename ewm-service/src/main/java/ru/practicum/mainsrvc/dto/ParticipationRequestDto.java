6941146cbff39340142f60071fdcb43c03f4d3f1
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-07-29 22:19:34 +0300

    fix checkstyle

--- ewm-service/src/main/java/ru/practicum/mainsrvc/dto/ParticipationRequestDto.java
+++ ewm-service/src/main/java/ru/practicum/mainsrvc/dto/ParticipationRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.mainsrvc.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public class ParticipationRequestDto {
@@ -18,17 +19,51 @@ public class ParticipationRequestDto {
 
     private UserShortDto requester;
 
-    public Long getId() { return id; }
-    public LocalDateTime getCreated() { return created; }
-    public String getStatus() { return status; }
-    public String getComment() { return comment; }
-    public EventShortDto getEvent() { return event; }
-    public UserShortDto getRequester() { return requester; }
+    public Long getId() {
+        return id;
+    }
 
-    public void setId(Long id) { this.id = id; }
-    public void setCreated(LocalDateTime created) { this.created = created; }
-    public void setStatus(String status) { this.status = status; }
-    public void setComment(String comment) { this.comment = comment; }
-    public void setEvent(EventShortDto event) { this.event = event; }
-    public void setRequester(UserShortDto requester) { this.requester = requester; }
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public EventShortDto getEvent() {
+        return event;
+    }
+
+    public UserShortDto getRequester() {
+        return requester;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setEvent(EventShortDto event) {
+        this.event = event;
+    }
+
+    public void setRequester(UserShortDto requester) {
+        this.requester = requester;
+    }
 }
